da3d10bdd2eab990069a3fd40fc9ec463984bd95
Dmitry Ponyatov <dponyatov@gmail.com>
2013-08-14 15:41:33 +0400

    + USERPKG.lib + sample routing ->LYT
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- Proteus/py.py
+++ Proteus/py.py
@@ -1,5 +1,5 @@
 import os
-for i in ['FTDI','USERDVC']:
+for i in ['FTDI','USERDVC','USERPKG']:
     C='cp "C:\Program Files (x86)\Labcenter Electronics\Proteus 7 Professional\LIBRARY\%s.lib" %s.lib'%(i,i)
     print C,os.system(C)
 raw_input('.')
